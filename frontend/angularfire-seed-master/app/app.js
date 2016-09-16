a880934f599facb894b9c11014cc1f0f328d28d3
Joel Gunzenreiner <j.gunzenreiner@gmail.com>
2016-09-16 23:47:46 +0200

    frontend

---
+++ frontend/angularfire-seed-master/app/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+    'myApp.config',
+    'myApp.security',
+    'myApp.home',
+    'myApp.account',
+    'myApp.chat',
+    'myApp.login'
+  ])
+  
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({
+      redirectTo: '/home'
+    });
+  }])
+  
+  .run(['$rootScope', 'Auth', function($rootScope, Auth) {
+    // track status of authentication
+    Auth.$onAuth(function(user) {
+      $rootScope.loggedIn = !!user;
+    });
+  }]);
