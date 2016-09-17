f36b84772490d8b77125eb47df530b79877dc653
Stefan Schaelle <schaelle@gmail.com>
2016-09-17 03:40:37 +0200

    cleanup

--- frontend/angularfire-seed-master/app/components/auth/auth.js
+++
@@ -1,4 +0,0 @@
-angular.module('firebase.auth', ['firebase', 'firebase.utils'])
-  .factory('Auth', ['$firebaseAuth', 'fbutil', function($firebaseAuth, fbutil) {
-    return $firebaseAuth(fbutil.ref());
-  }]);
