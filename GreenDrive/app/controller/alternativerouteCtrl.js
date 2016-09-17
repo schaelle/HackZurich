e69e1a9353af4dc0b3c0ee064a6db60379055f57
gdem <gdem@172.31.6.190>
2016-09-17 21:33:26 +0200

    Fixed data handling (getting user's carlist)

--- GreenDrive/app/controller/alternativerouteCtrl.js
+++ GreenDrive/app/controller/alternativerouteCtrl.js
@@ -1,29 +1,29 @@
-(function () {
-    app.controller("AlternativerouteController",
-	    function ($scope, $firebaseObject) {
+(function() {
+	app.controller("AlternativerouteController", function($scope, $firebaseArray, $firebaseObject, $stateParams) {
 
-      function parseRoute(input){
-        return input.split('|').map(i=>i.split(',').map(a=>parseFloat(a)));
-      }
+		function parseRoute(input){
+			return input.split('|').map(i=>i.split(',').map(a=>parseFloat(a)));
+		}
 
-			var ref = firebase.database().ref();
-			var logbox = $firebaseObject(ref.child('LogBox'));
+		$scope.userId = $stateParams.userId;
+		var ref = firebase.database().ref();
+		var user = $firebaseObject(ref.child('Users/' + $scope.userId));
+		user.$loaded().then(function(data) {
+			var logbox = $firebaseArray(ref.child('LogBox/' + data.Car + "/Data"));
 			var trips = [];
-	    	logbox.$loaded().then(function(data){
-	    		data.forEach(function(car) {
-	    			if(car)
-	    			for(var key in car.Data){
-	    				var record = car.Data[key];
-              record.Route = (parseRoute(record.Route));
-              record.Center = record.Route[0];
-	    				trips.push(record);
-	    			}
-	    		});
+			logbox.$loaded().then(function(data) {
+				data.forEach(function(car) {
+					if (car) {
+						var record = car;
+						record.Route = (parseRoute(record.Route));
+						record.Center = record.Route[0];
+						trips.push(record);
+					}
+				});
 			});
+			$scope.trackings = [];
+			$scope.trips = trips;			
+		});
 
-      $scope.trackings = [];
-
-	    	$scope.trips = trips;
-
-	    });
+	});
 })();
--- GreenDrive/app/controller/homeCtrl.js
+++ GreenDrive/app/controller/homeCtrl.js
@@ -1,69 +1,66 @@
 (function() {
-	app.controller('HomeController', function($scope, $firebaseObject, $stateParams) {
+	app.controller('HomeController', function($scope, $firebaseObject, $firebaseArray,
+			$stateParams) {
 
 		$scope.userId = $stateParams.userId;
-
-		 // get the reference
 		var ref = firebase.database().ref();
-    	var logbox = $firebaseObject(ref.child('LogBox'));
-    	logbox.$loaded().then(function(data){
+		var user = $firebaseObject(ref.child('Users/' + $scope.userId));
+		user.$loaded().then(function(data) {
+			var localCar = data;
+			$scope.car = data.Car;
+			var logbox = $firebaseArray(ref.child('LogBox/' + data.Car + "/Data"));
 
-    		var distance = 0;
-    		var mileage = 0;
-    		var emission_kg = 0;
+			logbox.$loaded().then(function(data) {
 
-    		// formula: https://www.spritmonitor.de/de/berechnung_co2_ausstoss.html
-    		var co2_petrol_kg_l = 2.33;
-    		var co2_diesel_kg_l = 2.64;
-    		var co2_naturalgas_kg_l = 2.79;
-    		var co2_biogas_kg_l = 1.64;
+				var distance = 0;
+				var mileage = 0;
+				var emission_kg = 0;
 
+				// formula:
+				// https://www.spritmonitor.de/de/berechnung_co2_ausstoss.html
+				var co2_petrol_kg_l = 2.33;
+				var co2_diesel_kg_l = 2.64;
+				var co2_naturalgas_kg_l = 2.79;
+				var co2_biogas_kg_l = 1.64;
 
+				data.forEach(function(car) {
 
-    		data.forEach(function(car) {
+					var co2_current_kg_l = 0;
 
-    			var co2_current_kg_l = 0;
+					switch (localCar.Fuel) {
+					case 1:
+						co2_current_kg_l = co2_petrol_kg_l;
+						break;
+					case 2:
+						co2_current_kg_l = co2_diesel_kg_l;
+						break
+					case 3:
+						co2_current_kg_l = co2_naturalgas_kg_l;
+						break;
+					case 4:
+						co2_current_kg_l = co2_petrol_kg_l;
+						break;
+					default:
+						co2_current_kg_l = co2_petrol_kg_l;
+					}
 
-    			switch (car.Fuel) {
-	    		    case 1:
-	    		    	co2_current_kg_l = co2_petrol_kg_l;
-	    		        break;
-	    		    case 2:
-	    		    	co2_current_kg_l = co2_diesel_kg_l;
-	    		        break
-	    		    case 3:
-	    		    	co2_current_kg_l = co2_naturalgas_kg_l;
-	    		        break;
-	    		    case 4:
-	    		    	co2_current_kg_l = co2_petrol_kg_l;
-	    		        break;
-	    		    default:
-	    		    	co2_current_kg_l = co2_petrol_kg_l;
-    			}
+					distance += car.Distance;
+					mileage += car.GasConsumption;
 
+					emission_kg = co2_current_kg_l * mileage;
 
-    			for(var key in car.Data){
-    				var record = car.Data[key];
-    				//console.log('Distance per event: ' + record.Distance + ' in km');
-    				//console.log('Fuel consumption per event: ' + record.Distance + ' in l');
-    				distance += record.Distance;
-    				mileage += record.GasConsumption;
-    			}
+				});
 
-    			emission_kg = co2_current_kg_l * mileage;
+				console.log("Total Distance: " + distance + ' in km');
+				console.log('Fuel consumption: ' + mileage + "  in l");
+				console.log('CO2 amount: ' + emission_kg / 1000 + 'in t');
 
-    		});
+				// Test data
+				$scope.emission = (emission_kg / 1000) * 100;
+				$scope.target = 100;
 
-    		console.log("Total Distance: " + distance + ' in km');
-    		console.log('Fuel consumption: '+ mileage + "  in l") ;
-    		console.log('CO2 amount: '+ emission_kg / 1000 + 'in t');
-
-            // Test data
-            $scope.emission = (emission_kg / 1000) * 100;
-            $scope.target = 100;
-
-    	});
+			});
+		});
 	});
-
 
 })();
