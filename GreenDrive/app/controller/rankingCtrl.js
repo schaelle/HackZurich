c81600c8be4ceecfd01fccdf0f3938036900c2de
Joel Gunzenreiner <j.gunzenreiner@gmail.com>
2016-09-17 12:18:35 +0200

    ranking ctrl + view

--- GreenDrive/app/controller/rankingCtrl.js
+++ GreenDrive/app/controller/rankingCtrl.js
@@ -1,9 +1,15 @@
 (function () {
-    app.controller('RankingController',
-    function ($scope) {
-
-
-    });
+    app.controller("RankingController", 
+	    function ($scope, $firebaseObject) {
+	    	 var ref = firebase.database().ref();
+	    // download physicsmarie's profile data into a local object
+	    // all server changes are applied in realtime
+	    	//$scope.profile = $firebaseObject(ref.child('profiles').child('physicsmarie'));
+	    	$scope.data = $firebaseObject(ref);
+	    	$scope.data.$loaded().then(function(cardata){
+	    		return cardata;	   
+	    	});
+	    });
 
 
 })();
